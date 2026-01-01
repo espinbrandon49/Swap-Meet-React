261156b2bed8c8541a2d74907f71112633644527
Brandon Espinosa <espinbrandon49@gmail.com>
2026-01-01 14:34:26 -0500

    Use JAWSDB_URL for Heroku database connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,18 +1,16 @@
-require('dotenv').config();
-const { Sequelize } = require('sequelize');
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PW,
-  {
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL, {
+    dialect: "mysql",
+    logging: false,
+  })
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: process.env.DB_HOST,
-    dialect: 'mysql',
+    dialect: "mysql",
     port: process.env.DB_PORT,
     logging: false,
-  }
-);
+  });
 
 module.exports = sequelize;
-
-
