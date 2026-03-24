b7fe85078b8c8817da98b700ea6cc440c04aff85
Brandon Espinosa <espinbrandon49@gmail.com>
2026-03-24 09:49:55 -0400

    update connection.js for production

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,16 +1,36 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL, {
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL, {
     dialect: "mysql",
-    logging: false,
-  })
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-    port: process.env.DB_PORT,
+    dialectOptions: {
+      ssl: { rejectUnauthorized: false },
+    },
     logging: false,
   });
+} else if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "mysql",
+    dialectOptions: {
+      ssl: { rejectUnauthorized: false },
+    },
+    logging: false,
+  });
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
+    {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT || 3306,
+      dialect: "mysql",
+      logging: false,
+    }
+  );
+}
 
 module.exports = sequelize;
